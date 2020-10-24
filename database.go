b56b5c9e5e0b306b6e41a1735abf2918eb0545ce
hpj <240088298@qq.com>
2020-10-24 17:24:59 +0800

    保存修改

--- database.go
+++ database.go
@@ -5,21 +5,26 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func CreateConnection() (*gorm.DB, error) {
 
 	// Get database details from environment variables
 	host := os.Getenv("DB_HOST")
+	host = "127.0.0.1:3306"
 	user := os.Getenv("DB_USER")
+	user = "jmu"
 	DBName := os.Getenv("DB_NAME")
+	DBName = "test"
 	password := os.Getenv("DB_PASSWORD")
+	password = "jmu"
 
+	dbDSN := "%s:%s@tcp(%s)/%s?charset=utf8"
 	return gorm.Open(
-		"postgres",
+		"mysql",
 		fmt.Sprintf(
-			"postgres://%s:%s@%s/%s?sslmode=disable",
+			dbDSN,
 			user, password, host, DBName,
 		),
 	)
